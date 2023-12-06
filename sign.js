ab14bb3d582c7c5db400f48df6ab7b31c369a87a
Durgeshmanitripathi <152366049+Durgeshmanitripathi@users.noreply.github.com>
2023-12-06 19:32:53 +0530

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,14 @@
+
+function submitForm() {
+    var eventName = document.getElementById('eventName').value;
+    // var dateTimeLocal = document.getElementById('dateTimeLocal').value;
+    var eventLocation = document.getElementById('eventLocation').value;
+    var eventType = document.getElementById('eventType').value;
+
+    // You can perform additional validation if needeed
+    // Display the submitted information (you can replace this with your desired action)
+    alert("Event Name: " + eventName + "\nEvent Date: " + eventTimeLocal + "\nEvent Location: " + eventLocation + "\nEvent Type: " + eventType);
+}
+function saving(){
+    alert("Thank you!")
+}
